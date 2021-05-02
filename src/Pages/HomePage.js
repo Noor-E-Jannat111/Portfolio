4dd9cc868108a272685c24b73c10b94ce80c180c
noorejannat111 <noorejannat188@gmail.com>
2021-05-02 14:57:41 +0600

    A lot of thinggs I have to changed.

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,204 @@
+import React from 'react'
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GithubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import YoutubeIcon from '@material-ui/icons/YouTube';
+import Particle from '../Components/Particle';
+
+
+function HomePage() {
+    return (
+        <HomePageStyled>
+            <div className="particle-con">
+                <Particle />
+            </div>
+            <div className="typography">
+                <h1>Hi, I'm <span>Noor-E-Jannat</span></h1>
+                <p>
+                    A passionate Web Developer. Experience of designing and building Web and Mobile application with JavaScript/React.js/Node.js and libraries some frameworks. 
+                </p>
+                 <div className="icons">
+                 <a href="https://codepen.io/pen/" className="icon i-facebook">
+                        <FacebookIcon />
+                    </a>
+                    
+                    <a href="https://github.com/Noor-E-Jannat111" className="icon i-github">
+                        <GithubIcon />
+                    </a>
+                    <a href="https://www.linkedin.com/in/noor-e-jannat-0490851b6/" className="icon i-linkedin">
+                        <LinkedInIcon />
+                    </a>
+                    <a href="https://codepen.io/pen/" className="icon i-youtube">
+                        <YoutubeIcon />
+                    </a>
+                </div> 
+
+{/* <div className="social-media-div">
+      {socialMediaLinks.github ? (
+        <a
+          href={socialMediaLinks.github}
+          className="icon-button github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.linkedin ? (
+        <a
+          href={socialMediaLinks.linkedin}
+          className="icon-button linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin-in"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.gmail ? (
+        <a
+          href={`mailto:${socialMediaLinks.gmail}`}
+          className="icon-button google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-google"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.gitlab ? (
+        <a
+          href={socialMediaLinks.gitlab}
+          className="icon-button gitlab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-gitlab"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.facebook ? (
+        <a
+          href={socialMediaLinks.facebook}
+          className="icon-button facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook-f"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.instagram ? (
+        <a
+          href={socialMediaLinks.instagram}
+          className="icon-button instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.twitter ? (
+        <a
+          href={socialMediaLinks.twitter}
+          className="icon-button twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-twitter"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.medium ? (
+        <a
+          href={socialMediaLinks.medium}
+          className="icon-button medium"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-medium"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.stackoverflow ? (
+        <a
+          href={socialMediaLinks.stackoverflow}
+          className="icon-button stack-overflow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-stack-overflow"></i>
+          <span></span>
+        </a>
+      ) : null}
+    </div> */}
+            </div>
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+   
+    .typography{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        width: 80%;
+
+        .icons{
+            display: flex;
+            justify-content: center;
+            margin-top: 1rem;
+            .icon{
+                border: 2px solid var(--border-color);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 50%;
+                transition: all .4s ease-in-out;
+                cursor: pointer;
+                &:hover{
+                    border: 2px solid var(--primary-color);
+                    color: var(--primary-color);
+                }
+                &:not(:last-child){
+                    margin-right: 1rem;
+                }
+                svg{
+                    margin: .5rem;
+                }
+            }
+
+            .i-youtube{
+                &:hover{
+                    border: 2px solid red;
+                    color: red;
+                }
+            }
+            .i-github{
+                &:hover{
+                    border: 2px solid #5F4687;
+                    color: #5F4687;
+                }
+            }
+        }
+    }
+`;
+
+export default HomePage;
